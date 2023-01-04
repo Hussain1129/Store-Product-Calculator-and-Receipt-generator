6dbed69eceb2f4d23da95cd8c6427f074fcaeecb
Hussain Ali <Hussainali54711@gmail.com>
2023-01-05 01:03:04 +0500

    Final Commit untill with modification

--- Store Product Calculator & Receipt Generator.py
+++ Store Product Calculator & Receipt Generator.py
@@ -9,41 +9,53 @@ def Recipt_calculate_gen():
     # Code to take user input and show the Sum of user inputs
     sum_price = 0
     user_list = []
+    products_name = {}
     print("\n\t\t\t\t\tWelcome to Hussain Store")
     Buywer_name = input('Hey welcome ! \nEnter your name here :')
     while True:
-        user = input("\nEnter price of thing else press q to Quict ... ")
+        user = input('\n---  If you want to Enter Product then press Any Key else Press q to Quit  ---  ')
 
         if (user != 'q'):
-            user_list.append(int(user))
-            sum_price = sum_price + int(user)
-            print(f"Till now you total bill is : {sum_price}")
+
+            product = input('Enter Product Name here !  ')
+            user_price = int(input("Enter price of your product !  "))
+
+            user_list.append(int(user_price))
+            sum_price = sum_price + int(user_price)
+            products_name[product] = user_price
+
+            print(f"\nTill now you total bill is : {sum_price}\n")
 
         else:
             print('You program has been quict :( ')
             print(f"\nYour total bill untill is : {sum_price}.RS")
             break
 
-    
+
     # Code to generate the Recipt of bill in Command line :
-
-    print('\nAnd Recipt of bill is :')
+    print('\nAnd Recipt of bill is :) ')
+    
     product_list = []
+    index = 1
+    
+    for item in products_name:
+        print(f"{index}. Price of '{item}' you enter is :   {products_name[item]}")
+        product_list.append(f"{index}. Price of '{item}' you enter is :   {products_name[item]}\n")
+        index += 1
+        
+    print(f"Sum of All Products and Bill is : {sum(user_list)}.RS")
 
-    for index, price in enumerate(user_list):
-        print(f"{index}. list of your bill is :  {price}")
-        product_list.append(f"{index}. List you Enter is :  {price}\n")
-    print(f"Sum of Bill is : {sum(user_list)}")
-
-
+   
     # Creating format of generating Script of bill ---
 
     store_name = 'Welcome to Hussain Store\n'
     bill_sum = f'Sum of Bill is : {sum(user_list)}'
 
+    
     # Code to generate Recipt of Bill in PDF form ---
 
     print("\nWould you like to see your Recipt in PDF file ?")
+    
     user_ask = input('yes / no \t').lower()
     if user_ask == 'yes':
         pdf = FPDF()
