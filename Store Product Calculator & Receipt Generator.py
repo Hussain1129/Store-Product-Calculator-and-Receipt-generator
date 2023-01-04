563b9cecca694aaeaf47d2b828f62150176e14da
Hussain Ali <Hussainali54711@gmail.com>
2023-01-05 00:01:54 +0500

    Added code to see Receipt in PDF form

--- Store Product Calculator & Receipt Generator.py
+++ Store Product Calculator & Receipt Generator.py
@@ -13,18 +13,16 @@ def Recipt_calculate_gen():
     Buywer_name = input('Hey welcome ! \nEnter your name here :')
     while True:
         user = input("\nEnter price of thing else press q to Quict ... ")
-        
+
         if (user != 'q'):
             user_list.append(int(user))
             sum_price = sum_price + int(user)
             print(f"Till now you total bill is : {sum_price}")
-            
+
         else:
             print('You program has been quict :( ')
             print(f"\nYour total bill untill is : {sum_price}.RS")
             break
-
-
 
     # Code to generate the Recipt of bill is :
 
@@ -36,38 +34,35 @@ def Recipt_calculate_gen():
         product_list.append(f"{index}. List you Enter is :  {price}\n")
     print(f"Sum of Bill is : {sum(user_list)}")
 
-    
-    
-    # # Format of generating Script of bill ---
+    # Format of generating Script of bill ---
 
-    # store_name = 'Welcome to Hussain Store\n'
-    # bill_sum = f'Sum of Bill is : {sum(user_list)}'
+    store_name = 'Welcome to Hussain Store\n'
+    bill_sum = f'Sum of Bill is : {sum(user_list)}'
 
-   
-    # # Code to generate Recipt of Bill in Text form ---
+    # Code to generate Recipt of Bill in Text form ---
 
-    # print("\nWould you like to see your Recipt in PDF file ?")
-    # user_ask = input('yes / no \t').lower()
-    # if user_ask == 'yes':
-    #     pdf = FPDF()
+    print("\nWould you like to see your Recipt in PDF file ?")
+    user_ask = input('yes / no \t').lower()
+    if user_ask == 'yes':
+        pdf = FPDF()
 
-    #     # Add a new page to the PDF
-    #     pdf.add_page()
+        # Add a new page to the PDF
+        pdf.add_page()
 
-    #     # Set the font and size
-    #     pdf.set_font("Arial", size=18)
+        # Set the font and size
+        pdf.set_font("Arial", size=18)
 
-    #     # Write the string to the PDF
-        
-    #     pdf.cell(200, 10, txt=store_name, ln=1)
-        
-    #     for line in product_list:
-    #         pdf.cell(200, 10, txt=line, ln=1)
-            
-    #     pdf.cell(200, 10, txt=bill_sum, ln=1)
+        # Write the string to the PDF
 
-    #     # Save the PDF to a file
-    #     pdf.output(Buywer_name+'.pdf')
+        pdf.cell(200, 10, txt=store_name, ln=1)
+
+        for line in product_list:
+            pdf.cell(200, 10, txt=line, ln=1)
+
+        pdf.cell(200, 10, txt=bill_sum, ln=1)
+
+        # Save the PDF to a file
+        pdf.output(Buywer_name+'.pdf')
 
 
 # Exrcution of code by main condition ---
